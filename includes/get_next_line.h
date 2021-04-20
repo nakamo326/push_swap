b409a62cf2ceef1f21230d6df848a75acc705f1c
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-04-21 00:43:15 +0900

    add norm v3 gnl

---
+++ includes/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ynakamot <ynakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 11:32:00 by ynakamot          #+#    #+#             */
+/*   Updated: 2021/04/20 23:23:47 by ynakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include "libft.h"
+
+# define SUCCESS 1
+# define ERROR -1
+# define CONTINUE 3
+# define END 0
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2047
+# endif
+
+int		get_next_line(int fd, char **line);
+
+#endif
