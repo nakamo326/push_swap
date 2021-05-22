5ed7971a7ff80782cdfc596dad5c9e3b382d06bd
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-05-22 09:39:37 +0900

    fix

--- src/push_swap/solve_half.c
+++ src/push_swap/solve_half.c
@@ -35,8 +35,8 @@ static void	sort_first_half(t_ps *ps, int start, int end)
 	if (*ps->b == NULL || ps->index >= end || start > end)
 		return ;
 	m = (end - start) / 2;
-	j = 0;
-	while (*ps->b != NULL && j + start < end)
+	j = start;
+	while (*ps->b != NULL && j < end)
 	{
 		if ((*ps->b)->val >= ps->list[start + m])
 			ps->ans = record_do(pa, ps->ans, ps->a, ps->b);
@@ -50,7 +50,7 @@ static void	sort_first_half(t_ps *ps, int start, int end)
 			ps->ans = record_do(rb, ps->ans, ps->a, ps->b);
 		j++;
 	}
-	sort_first_half(ps, start, m + start);
+	sort_first_half(ps, start, start + m);
 	sort_second_half(ps, end);
 }
 
