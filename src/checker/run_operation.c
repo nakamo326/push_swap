5fe2ca95e2badb13091e0213d79c4eee42fe147f
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-05-24 15:58:32 +0900

    validate op in read

--- src/checker/run_operation.c
+++ src/checker/run_operation.c
@@ -1,5 +1,58 @@
 #include "checker.h"
 
+//invalid operation
+
+static char	**get_list(char **list, bool *flag)
+{
+	int		ret;
+	char	*line;
+
+	ret = SUCCESS;
+	while (ret == SUCCESS)
+	{
+		ret = get_next_line(STDIN_FILENO, &line);
+		if (ret == END && (line == NULL || *line == '\0'))
+			break ;
+		if (ret == ERROR || is_valid_op(line) == err)
+		{
+			*flag = false;
+			free(line);
+			return (ft_free_split(list));
+		}
+		list = ft_addstr_split(list, line);
+		free(line);
+		if (list == NULL)
+			return (NULL);
+	}
+	free(line);
+	*flag = true;
+	return (list);
+}
+
+static bool	normal_run(t_stack **a, t_stack **b)
+{
+	int		i;
+	char	**list;
+	bool	ret;
+
+	list = NULL;
+	list = get_list(list, &ret);
+	if (ret == false)
+		return (false);
+	i = 0;
+	while (list && list[i] != NULL)
+	{
+		if (!do_operation(list[i], a, b))
+		{
+			ft_free_split(list);
+			return (false);
+		}
+		i++;
+	}
+	ft_free_split(list);
+	return (true);
+}
+
 static bool	debug_run(t_stack **a, t_stack **b)
 {
 	char	*line;
@@ -26,55 +79,6 @@ static bool	debug_run(t_stack **a, t_stack **b)
 	return (true);
 }
 
-static bool	get_list(char ***list)
-{
-	int		ret;
-	char	*line;
-
-	ret = SUCCESS;
-	while (ret == SUCCESS)
-	{
-		ret = get_next_line(STDIN_FILENO, &line);
-		if (ret == ERROR)
-		{
-			*list = ft_free_split(*list);
-			return (false);
-		}
-		if (ret == END && (line == NULL || *line == '\0'))
-		{
-			free(line);
-			break ;
-		}
-		*list = ft_addstr_split(*list, line);
-		free(line);
-		if (*list == NULL)
-			return (NULL);
-	}
-	return (true);
-}
-
-static bool	normal_run(t_stack **a, t_stack **b)
-{
-	int		i;
-	char	**list;
-
-	list = NULL;
-	if (!get_list(&list))
-		return (false);
-	i = 0;
-	while (list && list[i] != NULL)
-	{
-		if (!do_operation(list[i], a, b))
-		{
-			ft_free_split(list);
-			return (false);
-		}
-		i++;
-	}
-	ft_free_split(list);
-	return (true);
-}
-
 void	run_operation(t_stack **a, t_stack **b)
 {
 	bool	ret;
@@ -84,9 +88,6 @@ void	run_operation(t_stack **a, t_stack **b)
 	else
 		ret = normal_run(a, b);
 	if (ret == false)
-	{
-		output_error(a, b);
-		exit(EXIT_FAILURE);
-	}
+		exit(output_error(a, b));
 	return ;
 }
